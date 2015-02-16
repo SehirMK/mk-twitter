ad9da5bbbbfef7bfd156d2749d2779aeb67dbee9
hacerewr <hacertilbec@std.sehir.edu.tr>
2015-02-16 21:25:08 +0200

    views changed

--- twitterclone/twits/views.py
+++ twitterclone/twits/views.py
@@ -1,3 +1,15 @@
+#-*-coding:utf-8-*-
+from django.http import *
+from .models import *
 from django.shortcuts import render
+from django.shortcuts import render_to_response, get_object_or_404
+
+
 
 # Create your views here.
+def profile(request, user):
+	owner_user = get_object_or_404(User, username = user);
+	tweets = Twit.objects.filter(user = owner_user).order_by('-timestamp')
+	return render(request, 'own.html', {'tweets':tweets,})
+
+
--- twitterclone/twitterclone/urls.py
+++ twitterclone/twitterclone/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from twits.views import *
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'twitterclone.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^(?P<user>.*)/$', profile, name = "profile"),
 )
+
