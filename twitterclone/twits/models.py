ef73b498b7a13b546c56669876c657f45c5dc371
malisit <masit@outlook.com>
2015-02-16 16:22:25 +0200

    twit model added

--- twitterclone/twits/models.py
+++ twitterclone/twits/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Twit(models.Model):
+    user = models.ForeignKey(User, null=True, blank=True)
+    content = models.TextField(max_length=140)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    fav_count = models.IntegerField()
+    rt_count = models.IntegerField()
+    rt_users = models.ManyToManyField(User, related_name='rt_users')
+    fav_users = models.ManyToManyField(User, related_name='fav_users')
+
+
+    def __unicode__(self):
+        return self.content
